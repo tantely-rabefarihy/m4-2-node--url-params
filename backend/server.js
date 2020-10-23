e2cb2f7cde5331749c1627c6092631488c94fcab
Tantely Rabefarihy <tantelyrab@gmail.com>
2020-10-23 15:42:47 -0400

    backend done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const {top50} = require("./data/top50");
+
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -18,6 +21,83 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+.get("/top50", (req, res) => {
+  const data = top50;
+
+  
+  res.status(200).json({status:200, data})
+}
+)
+
+
+.get('/top50/song/:id', (req, res) => {
+  console.log("THIS IS THE ID",req.params.id);
+  const songRank = req.params.id;
+  const data = top50[songRank - 1];
+
+res.status(200).json({
+      status: 200, data
+    })
+  
+  })
+
+
+
+.get('/top50/artist/:artistName', (req, res) => {
+    console.log("THIS IS THE ARTIST",top50[1]);
+    const artist = req.params.artistName;
+    let data = [] ;
+  top50.filter(element => {
+    if(element.artist.toLowerCase() === artist){
+data.push(element)};
+  })
+  res.status(200).json({
+        status: 200, data
+      })
+    
+    })
+
+.get('/top50/popular-artist/', (req, res) => {
+
+      let artistsArr = [] ;
+    top50.filter(element => {
+      artistsArr.push(element.artist.toLowerCase())
+    });
+
+    const mostPopArtist = artistsArr.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+    
+    const MostPopularSongs = [];
+    top50.filter(song => {
+      if(song.artist.toLocaleLowerCase() === mostPopArtist)
+  {MostPopularSongs.push(song)}
+});
+
+
+    res.status(200).json({
+          status: 200, MostPopularSongs
+        })
+      
+      })
+
+
+
+.get("/top50/artist" , (req,res) => {
+const filteredArtist = top50.map(song => song.artist);
+console.log("ARTIST",filteredArtist )
+const AllArtists = [...new Set(filteredArtist)];
+
+  res.status(200).json({ status: 200, AllArtists})
+})
+
 
   // add new endpoints here ☝️
   // ---------------------------------
